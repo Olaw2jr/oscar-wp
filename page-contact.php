5c031ef82cfa9312cf6fbcfd0e8adf17e9a8244b
Olaw2jr <oscar.eugine@gmail.com>
2015-01-03 00:12:06 +0300

    Completed the contact us page

--- page-contact.php
+++ page-contact.php
@@ -12,8 +12,59 @@
  * @subpackage WeDesign
  * @since wedesign 1.0
  */
+?>
 
-get_header(); ?>
+<?php
+	$errors = array();
+	$isError = false;
+
+	$errorName = __( 'Please enter your name.', 'wedesign' );
+	$errorEmail = __( 'Please enter a valid email address.', 'wedesign' );
+	$errorMessage = __( 'Please enter the message.', 'wedesign' );
+
+	// Get the posted variables and validate them.
+	if ( isset( $_POST['is-submitted'] ) ) {
+		$name    = $_POST['cName'];
+		$email   = $_POST['cEmail'];
+		$message = $_POST['cMessage'];
+
+		// Check the name
+		if ( ! wedesign_validate_length( $name, 2 ) ) {
+			$isError             = true;
+			$errors['errorName'] = $errorName;
+		}
+
+		// Check the email
+		if ( ! is_email( $email ) ) {
+			$isError              = true;
+			$errors['errorEmail'] = $errorEmail;
+		}
+
+		// Check the message
+		if ( ! wedesign_validate_length( $message, 2 ) ) {
+			$isError                = true;
+			$errors['errorMessage'] = $errorMessage;
+		}
+
+		// If there's no error, send email
+		if ( ! $isError ) {
+			// Get admin email
+			$emailReceiver = get_option( 'admin_email' );
+
+			$emailSubject = sprintf( __( 'You have been contacted by %s', 'wedesign' ), $name );
+			$emailBody    = sprintf( __( 'You have been contacted by %1$s. Their message is:', 'wedesign' ), $name ) . PHP_EOL . PHP_EOL;
+			$emailBody    .= $message . PHP_EOL . PHP_EOL;
+			$emailBody    .= sprintf( __( 'You can contact %1$s via email at %2$s', 'wedesign' ), $name, $email );
+			$emailBody    .= PHP_EOL . PHP_EOL;
+			
+			$emailHeaders[] = "Reply-To: $email" . PHP_EOL;
+			
+			$emailIsSent = wp_mail( $emailReceiver, $emailSubject, $emailBody, $emailHeaders );
+		}
+	}
+?>
+
+<?php get_header(); ?>
 
 <!-- ==============================================
 		CONTACT
@@ -24,47 +75,70 @@ get_header(); ?>
 				
 				<div class="row">
 
-				<h1 class="section-title"><?php the_title(); ?></h1>
-				
+					<h1 class="section-title"><?php the_title(); ?></h1>
+					
 					<div class="col-sm-6 col-md-5 text-right scrollimation fade-up d1">
 						<?php if(have_posts()): while(have_posts()): the_post(); ?>
 							<?php the_content(); ?>
 						<?php endwhile; endif; ?>
 
 					</div>
-				
-					<!--=== Contact Form ===-->
 
-					<form id="contact-form" class="col-sm-6 col-md-offset-1 scrollimation fade-left d3" action="contact.php" method="post" novalidate>
-						
-						<div class="form-group">
-						  <label class="control-label" for="contact-name">Name</label>
-						  <div class="controls">
-							<input id="contact-name" name="contactName" placeholder="Your name" class="form-control requiredField" data-new-placeholder="Your name" type="text" data-error-empty="Please enter your name">
+					<div class="col-sm-6 col-md-offset-1 scrollimation fade-left d3">
+						<?php if ( isset( $emailIsSent ) && $emailIsSent ) : ?>
+							<div class="alert alert-success">
+								<?php _e( 'Your message has been sucessfully sent, thank you!', 'wedesign' ); ?>
+							</div> <!-- end alert -->
+						<?php else : ?>
+
+						<?php the_content(); ?>
+
+						<?php if ( isset( $isError ) && $isError ) : ?>
+							<div class="alert-alert-danger">
+								<?php _e( 'Sorry, it seems there was an error.', 'wedesign' ); ?>
+							</div> <!-- end alert -->
+						<?php endif; ?>
+						<?php endif; ?>
+
+
+						<!--=== Contact Form ===-->
+						<form action="<?php the_permalink(); ?>" id="contact-form" method="POST" role="form">
 							
-						  </div>
-						</div><!-- End name input -->
-						
-						<div class="form-group">
-						  <label class="control-label" for="contact-mail">Email</label>
-						  <div class=" controls">
-							<input id="contact-mail" name="email" placeholder="Your email" class="form-control requiredField" data-new-placeholder="Your email" type="email" data-error-empty="Please enter your email" data-error-invalid="Invalid email address">
+							<div class="form-group <?php if ( isset( $errors['errorName'] ) ) echo "has-error"; ?>">
+							  <label class="control-label" for="contact-name"><?php _e( 'Name', 'wedesign' ); ?></label>
+							  <div class="controls">
+								<input type="text" class="form-control" name="cName" placeholder="Your name" id="contact-name" value="<?php if ( isset( $_POST['cName'] ) ) { echo $_POST['cName']; } ?>">
+								<?php if ( isset( $errors['errorName'] ) ) : ?>
+									<p class="help-block"><?php echo $errors['errorName']; ?></p>
+								<?php endif; ?>
+							  </div>
+							</div><!-- End name input -->
 							
-						  </div>
-						</div><!-- End email input -->
-						
-						<div class="form-group">
-						  <label class="control-label" for="contact-message">Message</label>
-							<div class="controls">
-								<textarea id="contact-message" name="comments"  placeholder="Your message" class="form-control requiredField" data-new-placeholder="Your message" rows="6" data-error-empty="Please enter your message"></textarea>
-								
-							</div>
-						</div><!-- End textarea -->
-						
-						<p><button name="submit" type="submit" class="btn btn-default btn-lg" data-error-message="Error!" data-sending-message="Sending..." data-ok-message="Message Sent"><i class="fa fa-paper-plane"></i>Send Message</button></p>
-						<input type="hidden" name="submitted" id="submitted" value="true" />
-						
-					</form><!-- End contact-form -->
+							<div class="form-group <?php if ( isset( $errors['errorEmail'] ) ) echo "has-error"; ?>">
+							  <label class="control-label" for="contact-mail"><?php _e( 'Email Address', 'wedesign' ); ?></label>
+							  <div class=" controls">
+								<input type="text" class="form-control" placeholder="Your email" name="cEmail" id="contact-email" value="<?php if ( isset( $_POST['cEmail'] ) ) { echo $_POST['cEmail']; } ?>">
+								<?php if ( isset( $errors['errorEmail'] ) ) : ?>
+									<p class="help-block"><?php echo $errors['errorEmail']; ?></p>
+								<?php endif; ?>
+							  </div>
+							</div><!-- End email input -->
+							
+							<div class="form-group <?php if ( isset( $errors['errorMessage'] ) ) echo "has-error"; ?>">
+							  	<label class="control-label" for="contact-message"><?php _e( 'Message', 'wedesign' ); ?></label>
+								<div class="controls">
+									<textarea name="cMessage" class="form-control" placeholder="Your message" id="contact-message" cols="30" rows="10"><?php if ( isset( $_POST['cMessage'] ) ) { echo $_POST['cMessage']; } ?></textarea>
+									<?php if ( isset( $errors['errorMessage'] ) ) : ?>
+										<p class="help-block"><?php echo $errors['errorMessage']; ?></p>
+									<?php endif; ?>
+								</div>
+							</div><!-- End textarea -->
+
+							<input type="hidden" name="is-submitted" id="is-submitted" value="true">
+							<p><button type="submit" class="btn btn-default btn-lg"><i class="fa fa-paper-plane"></i><?php _e( 'Send Message', 'wedesign' ); ?></button></p>
+							
+						</form><!-- End contact-form -->
+					</div>
 					
 				</div>
 				
@@ -75,5 +149,4 @@ get_header(); ?>
 <?php
 // Gets footer.php
 get_footer();
-
 ?>
